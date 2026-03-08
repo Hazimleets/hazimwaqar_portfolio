6f17d9cf9fe291ea592908455257a984b12de7c5
Hazimleets <hazimansari88@gmail.com>
2026-03-08 11:50:24 +0500

    update portfolio, add .gitignore, remove node_modules tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Portfolio from './Portfolio'
+import Portfolio from './Portfolio_test'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
